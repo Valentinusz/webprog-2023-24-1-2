c58d941efc43153883ea5c1692d7ed0259bb78f8
Valentinusz <bb20020402@gmail.com>
2023-09-18 17:05:38 +0200

    add comments

--- 2/script.js
+++ 2/script.js
@@ -1,7 +1,12 @@
-// DOM (Document Object Model)
-// egy fa szerkezetű dokumentum (pl. SVG, XML, *HTML*) modellezése objektumokkal
-// JS esetében általában a HTML DOM-ra gondolunk DOM alatt
+// előadás: http://webprogramozas.inf.elte.hu/webprog/lectures/02/#/
+// feladatsor: http://webprogramozas.inf.elte.hu/#!/subjects/webprog-pti/gyak/02
+
+// 2. Gyakorlat DOM, eseménykezelés alapok
+
+// DOM (Document Object Model), egy fa szerkezetű dokumentum (pl. SVG, XML, *HTML*) modellezése objektumokkal
+// JS esetében általában a HTML DOM-ra gondolunk DOM alatta
 // a DOM kényelmes interfészt biztosít, a HTML dokumentum manipulálásához
+
 // objektumfa gyökere: document (globális változó)
 // fát Node objektumok alkotják
 // Node objektumnak 4 fajtája lehet
@@ -9,17 +14,30 @@
 // 2. Text (szöveges tartalom)
 // 3. Element (HTML elemek)
 // 4. Attribute (HTML elemek attribútumai)
-// CSS szelektorok ismétléséhez ajánlom a CSS Diner (https://flukeout.github.io/) oldalt
 
 console.log(document);
 
 // 1. Egy gomb megnyomására írd ki a dokumentum valamelyik általad választott részére, hogy "Helló világ!"!
+
+// a domban a querySelector metódussal tudunk keresni
+// egyetlen paramétere egy CSS szelektor
+// CSS szelektorok ismétléséhez ajánlom a CSS Diner (https://flukeout.github.io/) oldalt
+// az első olyan html elemet reprezentáló objektumot adja vissza, melyre illeszkedik a megadott css szelektor
+// ha nem illeszekedik semmire null
 const greetButton = document.querySelector('button#greet');
 console.log(greetButton);
 
-// eseménykezelés
+// eseménykezelő hozzáadása
+// következő gyakorlaton részletesebben
 greetButton.addEventListener('click', () => {
+    // egyszerűség kedvéért szúrjuk be a gomb után azt hogy helló
+    // nem ez az egyetlen mód, erősen helyzetfüggő mit érdemes használni
     greetButton.insertAdjacentText('beforeend', 'Helló világ!');
+    
+    // alapvetően három típusú beszúró művelet van
+    // 1. HTML pl. inserAdjacentHTML, a megadott szöveget HTML-ként értelmezi (óvakodjunk tőle)
+    // 2. Text pl. insertAdjacentText, a megadott szöveget sima szövegként szúrja be
+    // 3. Element pl. insertAdjacentElement, a megadott HTML elem objektumot szúrja be (preferáljuk)
 })
 
 // 2. Kérj be egy számot, és annyiszor írd ki egymás alá egyre növekvő betűméretekkel a "Hello világ!" szöveget!
@@ -28,17 +46,23 @@ const printCountInput = document.querySelector('input#print-count');
 const printButton = document.querySelector('button#print-button');
 
 printButton.addEventListener('click', () => {
+    // a value adattagon keresztül elérhető az input elemek tartalma
+    // value-ban stringe-ként szerepel az érték
+    // ha nem sikerül a parseolás, akkor count = NaN, ami hamis érték
     const count = Number.parseInt(printCountInput.value, 10);
 
     for (let index = 1; index <= count; index++) {
         // elem létrehozása
         const heading = document.createElement('h3');
+
+        // innerText = elem nyitó és záró tagje közötti szöveg
         heading.innerText = "Helló világ!";
 
-        // !mértékegység!
-        heading.style.fontSize = `${1 + index * 0.25}em`
+        // style adattagon keresztül elérhetők az elem inline stílusai
+        // kebab-case -> camelCase átírás 
+        heading.style.fontSize = `${1 + index * 0.25}em` // !mértékegység!
 
-        // szűlő gyerekei közé befűzés
+        // az adott elem utolsó gyerekeként szúrja be az adott elemet
         secondTaskDiv.appendChild(heading);
     }
 })
@@ -65,13 +89,16 @@ thirdTaskDiv.appendChild(table)
 // A gomb lenyomására másold át az egyik szöveges beviteli mező tartalmát a másikba!
 const fifthTaskDiv = document.querySelector('article:nth-of-type(5)');
 
-// elemen belül is lehet keresni
+// az Element típusú Node-okra is értelmezve van a querySelector
+// ekkor a querySelector a dom csak abban a részfában keres melynek gyökere az adott elem amire meghívjuk
+// figyeljünk arra, hogy ekkor nyilván a szelektorokat is ezt figyelembe véve kell megadnunk
 const firstInput = fifthTaskDiv.querySelector('label input');
 const secondInput = fifthTaskDiv.querySelector('label:nth-of-type(2) input');
 const copyButton = fifthTaskDiv.querySelector('button');
 console.log(firstInput, secondInput);
 
 copyButton.addEventListener('click', () => {
+    // üres input -> üres string logikai értéke hamis
     if (firstInput.value) {
         // value attribútum írható
         secondInput.value = firstInput.value;
@@ -83,6 +110,7 @@ copyButton.addEventListener('click', () => {
 // 7. Egy űrlapon csak akkor kérd be a leánykori nevet, ha nő az illető! Használd a rádiógombok click eseményét!
 // A megjelenítéshez, eltűntetéshez használd az elemek hidden tulajdonságát!
 const personForm = document.querySelector('form');
+
 const maleButton = personForm.querySelector('input[type="radio"]#male');
 const femaleButton = personForm.querySelector('input[type="radio"]#female');
 console.log(maleButton, femaleButton);
@@ -92,8 +120,8 @@ const maidenNameLabel = personForm.querySelector('label[for="maiden-name"]');
 console.log(maidenNameInput, maidenNameLabel);
 
 const toggleMaidenName = () => {
-    // checked = rádiógomb ki van-e választva
-    maidenNameInput.hidden = maleButton.checked;
+    // hidden tulajdonságon keresztül állítható, hogy megjelenjen-e az adott elem
+    maidenNameInput.hidden = maleButton.checked; // checked = rádiógomb ki van-e választva
     maidenNameLabel.hidden = maleButton.checked;
 }
 
@@ -103,12 +131,14 @@ femaleButton.addEventListener('click', toggleMaidenName);
 toggleMaidenName();
 
 // 8. Oldalbetöltéskor listázd ki az oldal összes hiperhivatkozásának a címét (href)!
-// NodeList != Tömb
+
+// querySelectorAll - összes selectornak megfelelő elem kiválasztása
+// NodeList-et vissza !EZ NEM EGY TÖMB!
 const links = document.querySelectorAll('a');
 const linksList = document.querySelector('ul#links');
 console.log(links);
 
-// kiválasztani hova akarjuk tenni a linkeket
+// bár nem tömb forEach ugyan úgy van (filter viszont már nincs)
 links.forEach(({href}) => {
     // végigmenni a linkeken és kiírni a címeket
     const item = document.createElement('li');
@@ -123,8 +153,9 @@ const counter = document.querySelector('h3#counter');
 const decrementButton = document.querySelector('button#dec');
 console.log(incrementButton, counter, decrementButton);
 
-const lowerBound = -5;
-const upperBound = 5;
+
+const lowerBound = -5; // alsó határ
+const upperBound = 5; // felső határ
 
 let counterValue = 0;
 
@@ -146,25 +177,32 @@ const decrement = () => {
     decrementButton.disabled = lowerBound === counterValue;
 }
 
+// eseménykezelők hozzáadása a gombokhoz
 incrementButton.addEventListener('click', increment);
 decrementButton.addEventListener('click', decrement);
+
+// ezt akár osztályba is lehet szervezni, vagy saját elemként definiálni
+// akit esetleg ez jobban érdekel: https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
+// de majd a későbbiekben lesz szó a kódszervezésről
 
 // 12. Készíts webes alkalmazást kamatos kamat számolására.
 // A számoláshoz meg kell adni a kiindulási összeget, a kamat értékét, valamint azt, hány évvel későbbi összegre vagyunk
 // kíváncsiak. Minden év végén adjuk hozzá a kamatot a tőkéhez, és a következő évben az képezi a kamatozás alapját.
 // A feladat során jelenítsük meg azt is, hogy melyik évben hogyan változik az összeg.
+
+// Bemenet
 const moneyInput = document.querySelector("input#money");
 const interestInput = document.querySelector("input#interest");
 const yearInput = document.querySelector("input#year");
-console.log(
-    moneyInput, interestInput, yearInput
-);
+console.log(moneyInput, interestInput, yearInput);
 
-const calculateButton = document.querySelector("button#calculate");
-console.log(calculateButton);
-
+// Kimenet
 const interestTableBody = document.querySelector("table#interestTable tbody");
 console.log(interestTableBody);
+
+// Eseménykezelés
+const calculateButton = document.querySelector("button#calculate");
+console.log(calculateButton);
 
 calculateButton.addEventListener('click', () => {
     // "állapottér"
@@ -172,8 +210,10 @@ calculateButton.addEventListener('click', () => {
     const interest = Number.parseFloat(interestInput.value, 10);
     const year = Number.parseInt(yearInput.value, 10);
 
+    // firstChild megadja az adott Node első gyerekét
+    // ezzel a trükkel eltávolítható az összes gyerek
     while (interestTableBody.firstChild) {
-        interestTableBody.firstChild.remove();
+        interestTableBody.firstChild.remove(); // remove() kiveszi azt a Node-t amit keghívunk a DOM-ból
     }
 
     for (let index = 0; index <= year; index++) {
@@ -202,6 +242,8 @@ calculateButton.addEventListener('click', () => {
 // b. Készíts egy legördülő mezőt, amelyben az egyes kiadók vannak felsorolva. Egy gombra kattintva táblázatos formában
 // jelenítsd meg a kiválasztott kiadóhoz tartozó könyveket!
 
+
+// adatok
 const books = [
     {
         szerzo: "J.K. Rowling",
@@ -239,3 +281,50 @@ const books = [
         ISBN: "978-0-7475-3849-0"
     }
 ];
+
+// a
+const filterButton = document.querySelector('button#filter');
+const releaseYearInput = document.querySelector('input#release');
+
+filterButton.addEventListener('click', () => {
+    const year = Number.parseInt(releaseYearInput.value, 10);
+    console.log(year)
+    console.log(books.filter(book => book.kiadasEve === year));
+})
+
+// b
+const selectPublisher = document.querySelector('select#publisher');
+const bookTable = document.querySelector('table#books');
+
+// halmaz típus
+const publisherSet = new Set();
+books.forEach(konyv => {
+    publisherSet.add(konyv.kiado);
+})
+
+publisherSet.forEach(publisher => {
+    const option = document.createElement('option');
+    option.value = publisher;
+    option.innerText = publisher;
+    selectPublisher.appendChild(option);
+})
+
+selectPublisher.addEventListener('change', () => {
+    // táblázat elemeknek lekérhető a törzse(i) a tBodies mezőn keresztül
+    while (bookTable.tBodies[0].firstChild) {
+        bookTable.tBodies[0].firstChild.remove();
+    }
+
+    books.forEach(book => {
+        if (book.kiado === selectPublisher.value) {
+            const row = document.createElement('tr');
+            row.appendChild(document.createElement('td'))
+            row.appendChild(document.createElement('td'))
+            row.appendChild(document.createElement('td'))
+            row.children[0].innerText = book.cim;
+            row.children[1].innerText = book.szerzo;
+            row.children[2].innerText = book.kiado;
+            bookTable.tBodies[0].appendChild(row);
+        }
+    })
+})
