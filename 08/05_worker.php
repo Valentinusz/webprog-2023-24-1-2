b5f8180963a098df09f6227c669cd050d2036ad1
Valentinusz <68827048+Valentinusz@users.noreply.github.com>
2023-11-13 09:09:14 +0100

    add pr8 starter

---
+++ 08/05_worker.php
@@ -0,0 +1,15 @@
+<?php
+
+$workers = [
+    ['name' => 'KING', 'job' => 'PRESIDENT', 'salary' => 5000],
+    ['name' => 'BLAKE', 'job' => 'MANAGER', 'salary' => 2850],
+    ['name' => 'CLARK', 'job' => 'MANAGER', 'salary' => 2450],
+    ['name' => 'JONES', 'job' => 'MANAGER', 'salary' => 2975],
+    ['name' => 'MARTIN', 'job' => 'SALESMAN', 'salary' => 1250],
+    ['name' => 'ALLEN', 'job' => 'SALESMAN', 'salary' => 1600],
+    ['name' => 'TURNER', 'job' => 'SALESMAN', 'salary' => 1500],
+    ['name' => 'JAMES', 'job' => 'CLERK', 'salary' => 950],
+    ['name' => 'WARD', 'job' => 'SALESMAN', 'salary' => 1250],
+    ['name' => 'FORD', 'job' => 'ANALYST', 'salary' => 3000]
+];
+?>
